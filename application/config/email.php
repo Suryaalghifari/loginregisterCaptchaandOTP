410353a6578f96eddbc42a4f43db70d3416b67af
UyaSky <m.suryaalghifari@gmail.com>
2025-08-22 14:26:30 +0700

    fix

--- application/config/email.php
+++ application/config/email.php
@@ -4,8 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['protocol']    = 'smtp';
 $config['smtp_host']   = 'smtp.gmail.com';
 $config['smtp_port']   = 587;
-$config['smtp_user']   = 'kademangantemp@gmail.com';     // ganti
-$config['smtp_pass']   = 'usgl asop qpag qsjk';            // gunakan App Password
+$config['smtp_user']   = '';     // ganti
+$config['smtp_pass']   = '';    // gunakan App Password
 $config['smtp_crypto'] = 'tls';
 $config['mailtype']    = 'text'; 
 $config['charset']     = 'utf-8';
